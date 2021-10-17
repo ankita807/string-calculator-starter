25ee6b9b89bae1a96120cb2a6f999df80bb3b982
ankita807 <mahajanankita828@gmail.com>
2021-10-17 13:15:47 +0530

    first test for return empty string  successful

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -3,7 +3,19 @@ package calculator;
 class StringCalculator {
 
     public int add(String input) {
+    	if(isEmpty(input)) {
         return 0;
+    	}
+    	return stringToInt(input);
     }
+    
+    private boolean isEmpty(String input) {
+    	return input.isEmpty();
+    }
+    
+    private int stringToInt(String input) {
+    	return Integer.parseInt(input);
+    }
+
 
 }
--- src/test/java/calculator/StringCalculatorShould.java
+++ src/test/java/calculator/StringCalculatorShould.java
@@ -6,15 +6,21 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class StringCalculatorShould {
 
-    @Test
-    void empty_string_should_return_0() {
-        StringCalculator stringCalculator = new StringCalculator();
-        assertEquals(0, stringCalculator.add(""));
-    }
-
-    @Test
-    void string_with_single_number_should_return_number_as_int() {
-        StringCalculator stringCalculator = new StringCalculator();
-        assertEquals(1, stringCalculator.add("1"));
-    }
+	private StringCalculator stringCalculator;
+	
+	 public StringCalculatorShould() {
+		 stringCalculator = new StringCalculator();
+	}
+	 
+   // below method will returns 0 if an empty string is passed
+   @Test
+   void empty_string_should_return_0() {
+       assertEquals(0, stringCalculator.add(""));
+   }
+   
+   // below method will returns an integer if passed a single number 
+   @Test
+   void string_with_single_number_should_return_number_as_int() {
+       assertEquals(1, stringCalculator.add("1"));
+   }
 }
